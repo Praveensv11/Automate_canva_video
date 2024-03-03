b2808e4cba1fba6c8f47b1244f8b0b1b0a661217
Praveen sv <112382643+Praveensv11@users.noreply.github.com>
2024-03-03 19:40:20 +0530

    Add files via upload

---
+++ youtubeauto.py
@@ -0,0 +1,47 @@
+import pyautogui, time
+
+time.sleep(6)
+workouts = ["20 Jumping Jacks","15 Push-Ups","20 Bodyweight Squats","30-Second Plank","10 Burpees"]
+locations = [[634, 976], [765, 985], [902, 982], [1036, 986], [1158, 978]]
+count = 0
+
+for location in locations:
+    pyautogui.moveTo(location[0], location[1])
+    time.sleep(2)
+    pyautogui.click(button="left")
+    time.sleep(1)
+    pyautogui.moveTo(830, 775)
+    pyautogui.click(button='left')
+    pyautogui.hotkey('ctrl', 'a')
+    pyautogui.hotkey("ctrl", "x")
+
+    pyautogui.click(button='left')
+    pyautogui.moveTo(285, 434)
+    time.sleep(1)
+    pyautogui.click(button="left")
+    time.sleep(1)
+    pyautogui.moveTo(1087, 573)
+    time.sleep(1)
+    pyautogui.click(button="left")
+    time.sleep(1)
+    pyautogui.hotkey('ctrl', 'a')
+    time.sleep(1)
+    pyautogui.typewrite(workouts[count])
+    count+=1
+
+
+#download 
+time.sleep(2)
+pyautogui.moveTo(1805, 161)
+pyautogui.click(button='left')
+time.sleep(4)
+pyautogui.moveTo(1600, 634)
+pyautogui.click(button='left')
+time.sleep(4)
+pyautogui.moveTo(1650, 594)
+pyautogui.click(button='left')
+
+
+
+# time.sleep(4)
+# print(pyautogui.position())
